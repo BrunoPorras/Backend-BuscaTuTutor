ecb7a7e5cd9512003fe59d69b02b392ab40e33c2
BrunoPorras <alefran2020@gmail.com>
2022-05-14 21:05:47 -0500

    Rutas de la api - Tutores

---
+++ src/routes/tutores.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const router = Router()
+const tutoresCtrl = require('../controllers/tutores.controller')
+
+//  Obtener todos los tutores
+router.get('/getTutores', tutoresCtrl.getAllTutores)
+
+//  Obtener un tutor
+router.get('/getTutor', tutoresCtrl.getOneTutor)
+
+//  Desactivar cuenta de tutor
+router.put('/unableTutor', tutoresCtrl.leaveTutor)
+
+//  Reactivar cuenta de tutor
+router.put('/enableTutor', tutoresCtrl.activeAccount)
+
+//  Filtrar tutores por especialidad o habilidadese
+router.get('/filterTutor',tutoresCtrl.filterBy)
+
+module.exports = router
